29418ac475784d67976719ea1d20e0b13669d6d2
carleyeshield <carlo_yamzon@yahoo.com>
2023-11-02 15:28:23 +0800

    Python Django Restaurant Menu

--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -8,11 +8,20 @@ MEAL_TYPE = (
     ('main_dishes', 'Main Dishes'),
     ('desserts', 'Desserts')
 )
+
+STATUS = (
+    (0, 'Unavailable'),
+    (1, 'Available'),
+)
+
 class Item(models.Model): #creating the columns in the table
     meal = models.CharField(max_length=1000, unique=True) #unique to avoid duplicate values
     description = models.CharField(max_length=2000) #no need to set unique because this variable can have duplicate values
     price = models.DecimalField(decimal_places=2)
     meal_type = models.CharField(choice=MEAL_TYPE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE) #if an author makes a meal, and if he is removed from the users, all his created items
+    author = models.ForeignKey(User, on_delete=models.PROTECT) #if an author makes a meal, and if he is removed from the users, all his created items
     # will be deleted, or if you want to save the meals created, you should use the code models.PROTECT, if you use models.SET_NULL,
     # the items or meals will be retained but the author will be set to a no value, if the author is deleted
+    status = models.IntegerField(choices=STATUS, default=0)
+    date_created = models.DateTimeField()
+    date_updated = models.DateTimeField()
