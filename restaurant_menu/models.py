5a7a253403c3f495653ea3a35f16fc36e85c02f3
carleyeshield <carlo_yamzon@yahoo.com>
2023-11-02 15:01:40 +0800

    kumite

--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model): #creating the columns in the table
+    meal = models.CharField(max_length=1000, unique=True) #unique to avoid duplicate values
+    description = models.CharField(max_length=1000) #no need to set unique because this variable can have duplicate values
