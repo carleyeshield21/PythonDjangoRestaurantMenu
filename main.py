f9add1ae80366f2c0e5709cd377c9dfd16a90b7f
carleyeshield <carlo_yamzon@yahoo.com>
2023-11-02 15:45:29 +0800

    Python Django Restaurant Menu

--- main.py
+++ main.py
@@ -18,4 +18,4 @@ def print_hi(name):
 # python manage.py startapp restaurant_menu
 # python manage.py makemigrations => required to create a directory for the database
 # python manage.py migrate => followed by this command to execute the python file inside the migrations directory, a db.sqlite3 file should be
-        # automatically created after executing this command
+        # automatically created after executing this command, this file can be viewed by using the DB Browser for SQLite
