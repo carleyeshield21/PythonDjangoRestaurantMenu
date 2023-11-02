759d9eb1cbbc6741b82017741740267cd978eca1
carleyeshield <carlo_yamzon@yahoo.com>
2023-11-02 15:17:48 +0800

    Python Django Restaurant Menu

--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -13,4 +13,5 @@ class Item(models.Model): #creating the columns in the table
     description = models.CharField(max_length=2000) #no need to set unique because this variable can have duplicate values
     price = models.DecimalField(decimal_places=2)
     meal_type = models.CharField(choice=MEAL_TYPE)
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #if someone makes a meal, and if he is removed from the users, all his created items
+    # will be deleted, or if you want to save the meals created, you should use the code models.PROTECT
