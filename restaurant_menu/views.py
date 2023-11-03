15c21a0945d62e637c864c54df53b5a53667ce9e
carleyeshield <carlo_yamzon@yahoo.com>
2023-11-03 09:29:32 +0800

    Python Django Restaurant Menu

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -5,6 +5,8 @@ from .models import Item
 # Create your views here.
 class MenuList(generic.ListView):
     queryset = Item.object.order_by('date_created') #imported from models.py, we can put a minus sign to reverse the order
+    template_name = 'index.html' #will be created in the directory
 
 class MenuItemDetail(generic.DetailView):
-    pass
+    model = Item
+    template_name = 'menu_item_detail.html' #will be created in the directory
