ecb5cc50950a51321db4553bb6a59e01c92fd16e
carleyeshield <carlo_yamzon@yahoo.com>
2023-11-03 09:03:05 +0800

    Python Django Restaurant Menu

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Item
 
 # Create your views here.
+class MenuList(generic.ListView):
+    queryset = Item.object.order_by('date_created') #imported from models.py, we can put a minus sign to reverse the order
+
+class MenuItemDetail(generic.DetailView):
+    pass
