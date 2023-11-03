d50aa8e357afe3916f94ecabdce146f4c68e68a7
carleyeshield <carlo_yamzon@yahoo.com>
2023-11-03 12:13:51 +0800

    Python Django Restaurant Menu

--- restaurant_menu/admin.py
+++ restaurant_menu/admin.py
@@ -3,3 +3,4 @@ from django.contrib import admin
 # Register your models here.
 class MenuItemAdmin(admin.ModelAdmin):
     list_display = ('meal', 'status') #from models.py class Item
+    list_filter = ('status',) #a comma is necessary for python to read it as a tuple, otherwise it will result to an error
